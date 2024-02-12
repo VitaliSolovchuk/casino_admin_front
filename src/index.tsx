a360119331380c6f511389e423c03bb1e9c06ebf
bogachev.v <bogachev.v@esplanade-ms.com>
2024-02-12 16:57:22 +0500

    fix layout styles, add react-query, fix Partners Table

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Index from './app';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <Index />
-  </React.StrictMode>,
+  </QueryClientProvider>,
 );
--- src/widgets/PartnersTable/PartnersTable.tsx
+++ src/widgets/PartnersTable/PartnersTable.tsx
@@ -1,12 +1,19 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, {
+  FC, useMemo, useState,
+} from 'react';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useQuery } from 'react-query';
 
-interface Partner {
-  partnerId: string;
+// import { data } from './fakeData';
+
+interface PartnerData {
+  partnerId: number;
   partnerName: string;
   currencyName: string;
+  uniquePlayers: Record<string, any>;
+  totalPlayers: number;
   sessionCount: number;
   totalActions: number;
   totalAmountBet: number;
@@ -14,50 +21,63 @@ interface Partner {
   totalProfit: number;
 }
 
-const columns: GridColDef[] = [
-  {
-    field: 'partnerName',
-    headerName: 'Partner Name',
-    flex: 1,
-    renderCell: (params) => (
-      <Link to={`/partners/${params.row.partnerId}`}>
-        {params.row.partnerName}
-      </Link>
-    ),
-  },
-  { field: 'currencyName', headerName: 'Currency Name', flex: 1 },
-  { field: 'sessionCount', headerName: 'Session Count', flex: 1 },
-  { field: 'totalActions', headerName: 'Total Actions', flex: 1 },
-  { field: 'totalAmountBet', headerName: 'Total Amount Bet', flex: 1 },
-  { field: 'totalAmountWin', headerName: 'Total Amount Win', flex: 1 },
-  { field: 'totalProfit', headerName: 'Total Profit', flex: 1 },
-];
+interface PaginationModel {
+  pageSize: number;
+  page: number;
+}
 
 const PartnersTable: FC = () => {
-  const [partnersData, setPartnersData] = useState<Partner[]>([]);
+  const { data, isLoading, error } = useQuery<PartnerData[]>(
+    'partners',
+    async () => {
+      const response = await axios.get<PartnerData[]>('https://dev.jetgames.io/admin-panel/partners');
+      return response.data;
+    },
+    { cacheTime: 10 * 60 * 1000 },
+  );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://dev.jetgames.io/admin-panel/partners');
-        setPartnersData(response.data);
-      } catch (error) {
-        console.error('Error fetching partners data:', error);
-      }
-    };
+  const [paginationModel, setPaginationModel] = useState<PaginationModel>({
+    pageSize: 25,
+    page: 0,
+  });
 
-    fetchData();
-  }, []);
+  const columns: GridColDef[] = useMemo(() => [
+    {
+      field: 'partnerName',
+      headerName: 'Partner Name',
+      flex: 1,
+      renderCell: (params) => (
+        <Link to={`/partners/${params.row.partnerId}`}>
+          {params.row.partnerName}
+        </Link>
+      ),
+    },
+    { field: 'currencyName', headerName: 'Currency Name', flex: 1 },
+    { field: 'sessionCount', headerName: 'Session Count', flex: 1 },
+    { field: 'totalActions', headerName: 'Total Actions', flex: 1 },
+    { field: 'totalAmountBet', headerName: 'Total Amount Bet', flex: 1 },
+    { field: 'totalAmountWin', headerName: 'Total Amount Win', flex: 1 },
+    { field: 'totalProfit', headerName: 'Total Profit', flex: 1 },
+  ], []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) {
+    return (
+      <div>
+        Error fetching partners data:
+        {(error as Error).message}
+      </div>
+    );
+  }
   return (
-    <div>
+    <div style={{ width: '100%' }}>
       <DataGrid
-        rows={partnersData}
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
+        rows={data || []}
         columns={columns}
-        // pageSize={25}
-        // rowsPerPageOptions={[10, 25, 50, 100]}
         pagination
         autoHeight
-        // disableSelectionOnClick
         getRowId={(row) => `${row.partnerId}-${row.currencyName}`}
       />
     </div>
