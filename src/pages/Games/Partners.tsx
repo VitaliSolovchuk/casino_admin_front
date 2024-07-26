22773f159b884f802daf8167452a70f9e3aca713
vitali.s <vitalisolovchuk@gmail.com>
2024-07-26 21:22:28 +0300

    adde games page

---
+++ src/pages/Games/Partners.tsx
@@ -0,0 +1,168 @@
+import React, {
+  FC, useEffect, useMemo, useRef,
+} from 'react';
+import { GridColDef } from '@mui/x-data-grid';
+import { useNavigate } from 'react-router-dom';
+import TableGrid from 'widgets/tableGrid/ui/TableGrid';
+import useTableGrid from 'widgets/tableGrid/model/tableGridStore';
+import useFilterDateRange from 'entities/dateRangeCalendar/model/dateRangeStore';
+import { useDataRequest } from 'shared/lib/hooks/useDataRequest';
+import { PartnerData } from 'features/partners/types/types';
+import { useQueryClient } from 'react-query';
+import { postPartnersData } from 'features/partners/api';
+// import { paths } from 'shared/lib/consts/paths';
+import { useMutationRequest } from 'shared/lib/hooks/useMutationRequest';
+
+interface Row {
+  partnerId: number;
+  currencyName: string;
+}
+
+const Games: FC = () => {
+  const {
+    filterModel,
+    sortModel,
+    paginationModel,
+  } = useTableGrid((state) => state);
+
+  const {
+    filterDate,
+  } = useFilterDateRange((state) => state);
+
+  const { dateRange } = filterDate;
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const isFirstRender = useRef(true);
+
+  const {
+    data,
+    isLoading,
+    error,
+  } = useDataRequest<PartnerData>(
+    'games',
+    () => postPartnersData({
+      paginationModel,
+      sortModel,
+      filterModel,
+      filterDate: {
+        startDate: dateRange[0],
+        endDate: dateRange[1],
+      },
+    }),
+  );
+
+  const { mutate } = useMutationRequest<PartnerData>(
+    'games',
+    () => postPartnersData(
+      {
+        paginationModel,
+        sortModel,
+        filterModel,
+        filterDate: {
+          startDate: dateRange[0],
+          endDate: dateRange[1],
+        },
+      },
+    ),
+  );
+
+  useEffect(() => {
+    if (!isFirstRender.current) {
+      mutate();
+    } else {
+      isFirstRender.current = false;
+    }
+  }, [mutate, paginationModel, sortModel, filterModel, filterDate, dateRange]);
+
+  // {
+  //   "totalAmountBet": "1.6200",
+  //   "totalAmountWin": "3.1200",
+  //   "totalUniquePlayers": 2,
+  //   "totalSessions": 2,
+  //   "totalActions": 2,
+  //   "partnerName": "coinsbet",
+  //   "currencyName": "USD",
+  //   "partnerId": 5,
+  //   "currencyId": 1,
+  //   "date": "2024-07-18T00:00:00.000Z",
+  //   "totalGGR": "-1.500",
+  //   "totalGGRUSD": "-1.5",
+  //   "RTP": "192.59",
+  //   "totalAmountBetUSD": "1.62",
+  //   "totalAmountWinUSD": "3.12"
+  // }
+
+  const columns: GridColDef[] = useMemo(() => [
+    {
+      field: 'partnerName',
+      headerName: 'Partner',
+      flex: 1,
+    },
+    {
+      field: 'currencyName',
+      headerName: 'Currency',
+      flex: 1,
+    },
+    {
+      field: 'totalUniquePlayers',
+      headerName: 'Players',
+      flex: 1,
+    },
+    {
+      field: 'totalSessions',
+      headerName: 'Sessions',
+      flex: 1,
+    },
+    {
+      field: 'totalActions',
+      headerName: 'Actions',
+      flex: 1,
+    },
+    {
+      field: 'totalAmountBetUSD',
+      headerName: 'Total Bet',
+      flex: 1,
+    },
+    {
+      field: 'totalAmountWinUSD',
+      headerName: 'Total Win',
+      flex: 1,
+    },
+    // {
+    //   field: 'totalProfit',
+    //   headerName: 'Total Profit',
+    //   flex: 1,
+    // },
+    {
+      field: 'totalGGRUSD',
+      headerName: 'Total Profit USD',
+      flex: 1,
+    },
+    {
+      field: 'RTP',
+      headerName: 'RTP %',
+      flex: 1,
+    },
+  ], []);
+  // const handleRowClick = (row: Record<string, number>) => {
+  //   if (row.partnerId) {
+  //     queryClient.invalidateQueries({ queryKey: 'players' })
+  //       .then(() => navigate(`${paths.players}/?id=${row.partnerId}&currency=${row.currencyName}`));
+  //   }
+  // };
+  const rowId = (row: Row) => `${row.partnerId}-${row.currencyName}`;
+  return (
+    <div>
+      <TableGrid
+        data={data?.partnerCurrencyStatistic}
+        rowId={rowId}
+        isLoading={isLoading}
+        error={error as Error}
+        columns={columns}
+        // handleRowClick={handleRowClick}
+        title="Games Table"
+      />
+    </div>
+  );
+};
+export default Games;
