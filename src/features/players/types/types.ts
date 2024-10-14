9735bf577b1da0a824c9d1007c8e41ce89afe452
vitali.s <vitalisolovchuk@gmail.com>
2024-10-14 21:03:32 +0300

    added Sessions2

--- src/features/players/types/types.ts
+++ src/features/players/types/types.ts
@@ -1,8 +1,6 @@
 import { GridFilterModel } from '@mui/x-data-grid';
-import { GridSortModel } from '@mui/x-data-grid/models/gridSortModel';
 import { GridPaginationModel } from '@mui/x-data-grid/models/gridPaginationProps';
 import { Dayjs } from 'dayjs';
-import { DateRange } from '@mui/x-date-pickers-pro';
 
 export interface Player {
   playerId: string;
@@ -13,18 +11,6 @@ export interface Player {
   totalAmountBet: number;
   totalAmountWin: number;
   totalProfit: number;
-}
-
-export interface getPlayersProps {
-  partnerId: string | null,
-  params: {
-    filterModel: GridFilterModel,
-    sortModel: GridSortModel
-    paginationModel: GridPaginationModel,
-    filterDate: {
-      dateRange: DateRange<Dayjs>
-    },
-  }
 }
 
 export interface postPlayersProps {
--- src/pages/Sessions2/Sessions2.tsx
+++ src/pages/Sessions2/Sessions2.tsx
@@ -9,47 +9,44 @@ import { GridColDef, GridSortModel } from '@mui/x-data-grid';
 import useTableGrid from 'widgets/tableGrid/model/tableGridStore';
 import useFilterDateRange from 'entities/dateRangeCalendar/model/dateRangeStore';
 import { useDataRequest } from 'shared/lib/hooks/useDataRequest';
-import { Player } from 'features/players/types/types';
-import { postPlayersData } from 'features/players/api';
 import { useQueryClient } from 'react-query';
 import { paths } from 'shared/lib/consts/paths';
 import { useMutationRequest } from 'shared/lib/hooks/useMutationRequest';
-import TableGridLocalSort from 'widgets/tableGrid/ui/TableGridLocalSort';
+// import TableGridLocalSort from 'widgets/tableGrid/ui/TableGridLocalSort';
+import { postSessionsCurrencyData } from 'features/sessions-currency-parner/api';
+import { ResultSessionsInfo, SessionInfo } from 'features/sessions-currency-parner/types/types';
+import TableGridLocalSort from 'pages/SearchPlayerUserSessions/TableGridLocalSort';
 
 interface Row {
   partnerId: string;
   playerId: string;
   sessionId: string;
 }
-
-const Players: FC = () => {
+const Sessions2: FC = () => {
   const { search, state } = useLocation();
   const params = new URLSearchParams(search);
-  const partnerId = params.get('id');
-  const currency = params.get('currency');
+  const partnerId = params.get('id') as string;
+  const currency = params.get('currency') as string;
 
   const navigate = useNavigate();
   const {
     filterModel,
-    // sortModel,
     paginationModel,
   } = useTableGrid((state) => state);
   const { filterDate } = useFilterDateRange((state) => state);
   const { dateRange } = filterDate;
   const queryClient = useQueryClient();
-  // const isFirstRender = useRef(true);
 
   const {
     data,
     isLoading,
     error,
-  } = useDataRequest<Player[]>(
-    'players',
-    () => postPlayersData({
+  } = useDataRequest<ResultSessionsInfo>(
+    'sessions',
+    () => postSessionsCurrencyData({
       partnerId,
-      currency,
+      currencyes: currency,
       paginationModel,
-      // sortModel,
       filterModel,
       filterDate: {
         startDate: dateRange[0],
@@ -58,13 +55,12 @@ const Players: FC = () => {
     }),
   );
 
-  const { mutate } = useMutationRequest<Player[]>(
-    'players',
-    () => postPlayersData({
+  const { mutate, isLoading: isLoadingMutate } = useMutationRequest<ResultSessionsInfo>(
+    'sessions',
+    () => postSessionsCurrencyData({
       partnerId,
-      currency,
+      currencyes: currency,
       paginationModel,
-      // sortModel,
       filterModel,
       filterDate: {
         startDate: dateRange[0],
@@ -80,16 +76,15 @@ const Players: FC = () => {
   const [sortModel, setSortModel] = useState<GridSortModel>([{ field: 'ggrUsd', sort: 'desc' }]);
 
   const sortedData = useMemo(() => {
-    if (!data || data.length === 0) return [];
-    if (!sortModel || sortModel.length === 0) return data;
+    if (!data || data.items.length === 0) return [];
+    if (!sortModel || sortModel.length === 0) return data.items;
 
     const { field, sort } = sortModel[0];
 
-    const sorted = [...data].sort((a, b) => {
-      let valueA = a[field as keyof Player];
-      let valueB = b[field as keyof Player];
+    const sorted = [...data.items].sort((a, b) => {
+      let valueA = a[field as keyof SessionInfo];
+      let valueB = b[field as keyof SessionInfo];
 
-      // eslint-disable-next-line max-len
       const isNumeric = (val: any) => typeof val === 'number' || (typeof val === 'string' && !Number.isNaN(parseFloat(val)) && Number.isFinite(val));
 
       if (isNumeric(valueA)) {
@@ -116,11 +111,13 @@ const Players: FC = () => {
     { field: 'playerId', headerName: 'Player ID', flex: 1 },
     { field: 'sessionId', headerName: 'Session ID', flex: 1 },
     { field: 'currencyName', headerName: 'Currency', flex: 1 },
-    { field: 'gameName', headerName: 'Game Name', flex: 1 },
+    { field: 'gameName', headerName: 'Game', flex: 1 },
     { field: 'totalActions', headerName: 'Actions', flex: 1 },
-    { field: 'totalAmountBetUSD', headerName: 'Total Bet', flex: 1 },
-    { field: 'totalAmountWinUSD', headerName: 'Total Win', flex: 1 },
-    { field: 'totalGGRUSD', headerName: 'Total Profit USD', flex: 1 },
+    { field: 'totalAmountBetUsd', headerName: 'Total Bet (Usd)', flex: 1 },
+    { field: 'totalAmountWinUsd', headerName: 'Total Win (Usd)', flex: 1 },
+    { field: 'totalGgrUsd', headerName: 'Profit (Usd)', flex: 1 },
+    { field: 'Rtp', headerName: 'Rtp', flex: 1 },
+    { field: 'firstBetTime', headerName: 'Time', flex: 1 },
   ], []);
 
   const rowId = (row: Row) => `${row.partnerId}-${row.playerId}-${row.sessionId}`;
@@ -130,16 +127,18 @@ const Players: FC = () => {
         .then(() => navigate(`${paths.sessionEvents}/?id=${row.sessionId}`, { state: { ...state, Players: search } }));
     }
   };
+
   return (
     <div>
       <TableGridLocalSort
         data={sortedData}
+        rowCountState={data?.totalItemsCount} // Пропс добавлен в интерфейс
         rowId={rowId}
-        isLoading={isLoading}
+        isLoading={isLoading || isLoadingMutate}
         error={error as Error}
         columns={columns}
         handleRowClick={handleRowClick}
-        title="Players Table"
+        title="Player-session Table"
         sortModel={sortModel}
         onSortModelChange={handleSortChange}
       />
@@ -147,4 +146,4 @@ const Players: FC = () => {
   );
 };
 
-export default Players;
+export default Sessions2;
