2fc89a3b0b027a1469882e2642b309915790a300
vitali.s <vitalisolovchuk@gmail.com>
2025-05-06 10:52:33 +0300

    ref

--- src/features/currency-games/api/index.ts
+++ src/features/currency-games/api/index.ts
@@ -1,13 +1,11 @@
-import axios from 'axios';
 import { baseURL } from 'shared/lib/consts/url';
+import $api from '../../../http';
 import { CurrencyGamesDataProps, CurrencyGamesData } from '../types/types';
 
 export const postCurrencyGamesStatisticData = async (props: CurrencyGamesDataProps) => {
   try {
-    const response = await axios.post<CurrencyGamesData>(
-      `${baseURL}/admin-panel-statistics/get-grouped-by-currency-games`,
-      props,
-    );
+    // eslint-disable-next-line max-len
+    const response = await $api.post<CurrencyGamesData>(`${baseURL}/admin-panel-statistics/get-grouped-by-currency-games`, props);
     return response.data;
   } catch (error) {
     console.error(error);
